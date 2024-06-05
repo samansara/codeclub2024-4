524093a13a4c04f5394a1ba07d3184233124b9c7
Samuel Ansara <sansara@umich.edu>
2024-06-05 15:32:51 -0400

    Added poem route with AI

--- app/poem/route.js
+++ app/poem/route.js
@@ -6,15 +6,19 @@ export async function GET(request) {
   const region = request.headers.get("cf-region")
   const city = request.headers.get("cf-ipcity")
 
+  //Add an error if city or region not read
+
   if (!city || !region) {
     return new Response("City or Region is null", 403)
   }
 
+  //Generate an AI response
   const answer = await getRequestContext().env.AI.run("@cf/meta/llama-3-8b-instruct", {
     prompt: `Can you write me a funny poem about ${city}, ${region}?`,
     stream: true
   })
 
+  //Return an error if no AI response
   if (!answer) {
     return new Response("AI failed to return a response", 403)
   }
