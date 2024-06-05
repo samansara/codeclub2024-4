032ea5bf8a38cc5e906d2915fff4893626d4b44c
Samuel Ansara <sansara@umich.edu>
2024-06-05 15:01:14 -0400

    Adding error handling for poem route

--- app/poem/route.js
+++ app/poem/route.js
@@ -6,6 +6,10 @@ export async function GET(request) {
   const region = request.cf.region
   const city = request.cf.city
 
+  if (!city || !region) {
+    return new Response("City or Region is null", 403)
+  }
+
   const answer = await env.AI.run("@cf/meta/llama-3-8b-instruct", {
     prompt: `Can you write me a funny poem about ${city}, ${region}?`,
     stream: true
