74b27603b4f960c44327bb4ffcd4bcc9fd33ff08
Samuel Ansara <sansara@umich.edu>
2024-06-05 15:06:56 -0400

    attempting to fix internal server error

--- app/poem/route.js
+++ app/poem/route.js
@@ -5,12 +5,13 @@ export const runtime = 'edge'
 export async function GET(request) {
   const region = request.cf.region
   const city = request.cf.city
+  const AI = getRequestContext().env.AI
 
   if (!city || !region) {
     return new Response("City or Region is null", 403)
   }
 
-  const answer = await env.AI.run("@cf/meta/llama-3-8b-instruct", {
+  const answer = await AI.run("@cf/meta/llama-3-8b-instruct", {
     prompt: `Can you write me a funny poem about ${city}, ${region}?`,
     stream: true
   })
