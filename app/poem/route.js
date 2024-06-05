47f8d7929c992fac9a27d9e40cfa28f75cc7c118
Samuel Ansara <sansara@umich.edu>
2024-06-05 15:09:54 -0400

    added another error handler

--- app/poem/route.js
+++ app/poem/route.js
@@ -5,16 +5,19 @@ export const runtime = 'edge'
 export async function GET(request) {
   const region = request.cf.region
   const city = request.cf.city
-  const AI = getRequestContext().env.AI
 
   if (!city || !region) {
     return new Response("City or Region is null", 403)
   }
 
-  const answer = await AI.run("@cf/meta/llama-3-8b-instruct", {
+  const answer = await env.AI.run("@cf/meta/llama-3-8b-instruct", {
     prompt: `Can you write me a funny poem about ${city}, ${region}?`,
     stream: true
   })
+
+  if (!answer) {
+    return new Response("AI failed to return a response")
+  }
 
   return new Response(answer, {
     headers: {
